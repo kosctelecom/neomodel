6684a41ff8b2b20165731d8f48db77345f545020
bruno.dupuis <bruno.dupuis@kosc-telecom.fr>
2021-01-18 17:25:56 +0100

    add a setting wanted by django-neomodel

--- neomodel/config.py
+++ neomodel/config.py
@@ -3,3 +3,5 @@ DATABASE_URL = 'bolt://neo4j:foobar@localhost:7687'
 FORCE_TIMEZONE = False
 ENCRYPTED_CONNECTION = False
 MAX_CONNECTION_POOL_SIZE = 50
+# alias as django_neomodel needs this
+MAX_POOL_SIZE = 50
